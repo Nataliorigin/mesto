a74ecae1c7e2c519ad3a54e3af1a07796b031c62
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-11-28 08:57:15 +0500

    .

--- scripts/index.js
+++ scripts/index.js
@@ -26,32 +26,32 @@ const popupElementImgCaption = document.querySelector('.popup__img-caption');
 const initialItems = [
   {
     name: 'Вкусный ужин',
-    link: '../img/krakenimages.jpg',
+    link: './img/krakenimages.jpg',
     alt: 'Праздничный ужин'
   },
   {
     name: 'Ароматная ёлка',
-    link: '../img/alsu-vershinina.jpg',
+    link: './img/alsu-vershinina.jpg',
     alt: 'Украшенная елка'
   },
   {
     name: 'Подарки',
-    link: '../img/anita-austvika-podarok.jpg',
+    link: './img/anita-austvika-podarok.jpg',
     alt: 'Подарки'
   },
   {
     name: 'Радость',
-    link: '../img/anita-austvika-girl.jpg',
+    link: './img/anita-austvika-girl.jpg',
     alt: 'Девочка наряжает елку'
   },
   {
     name: 'Нарядный город',
-    link: '../img/anita-austvika.jpg',
+    link: './img/anita-austvika.jpg',
     alt: 'Нарядный город'
   },
   {
     name: 'Настроение',
-    link: '../img/katie-azi-.jpg',
+    link: './img/katie-azi-.jpg',
     alt: 'Настроение'
   }
 ];
